dd50b03c98c47189aca70e4acad77a28b07ff2f9
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-11-21 17:31:43 +0100

    Home footer created. Needs revision.

--- views/Home.jsx
+++ views/Home.jsx
@@ -4,7 +4,14 @@ const Layout = require("./Layout");
 function Home() {
   return (
     <Layout title="Home">
-      
+    {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> */}
+      <footer>
+        <ul>
+          <li><a href="" target="_blank"><i class="fa fa-facebook fa-fw"></i></a></li>
+          <li><a href="" target="_blank"><i class="fa fa-instagram fa-fw"></i></a></li>
+        </ul>
+        <p>MarsGenX © 2020</p>
+      </footer>
     </Layout>
   );
 }
