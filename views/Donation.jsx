478eba3e9404b85dc2211b3663cc527532d34ba7
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-11-22 16:39:42 +0100

    By the moment, font awesome put in stand by. Basic html structure created for the member page. Some icon images added to the images folder

--- views/Donation.jsx
+++ views/Donation.jsx
@@ -1,19 +1,14 @@
 const React = require("react");
 const Layout = require('./Layout')
 
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-const element = <FontAwesomeIcon icon={faCoffee} />
-
-ReactDOM.render(element, document.body)
-
 
 
 function Donation() {
   return (
     <Layout title="Donation">
+      <main>
+        <h1>Donation Page</h1>
+      </main>
 
     </Layout>
   )
--- views/Home.jsx
+++ views/Home.jsx
@@ -1,14 +1,6 @@
 const React = require("react");
 const Layout = require("./Layout");
 
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-const element = <FontAwesomeIcon icon={faCoffee} />
-
-ReactDOM.render(element, document.body)
-
 
 function Home() {
   return (
