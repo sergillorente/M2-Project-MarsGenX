d1636fcdd479d951b7d64fd56aac76d247913164
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-11-22 14:09:58 +0100

    Importing fontawesome to add specific icons. The importation will be affecting Home page, Member page, Profile page and Donation page

--- views/Profile.jsx
+++ views/Profile.jsx
@@ -1,6 +1,16 @@
 const React = require("react");
 const Layout = require('./Layout')
 
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faCoffee} />
+
+ReactDOM.render(element, document.body)
+
+
+
 function Profile() {
   return (
     <Layout title="Profile">
