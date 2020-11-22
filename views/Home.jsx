ea145b03ccb8367d4fd13f4de6eb7882853240c9
salvamoro <sall.morise@gmail.com>
2020-11-22 17:53:23 +0100

    home and progile page layout

--- views/Home.jsx
+++ views/Home.jsx
@@ -4,10 +4,32 @@ const Layout = require("./Layout");
 function Home() {
   return (
     <Layout title="Home">
+      <body>
+        <h1> What is MarsGenX?</h1>
+        <br></br>
+        <div className="image">
+          <img src="../public/images/homepage-image.jpg" />
+        </div>
+        <p>
+          {" "}
+          The main purpose of MarsGenX is to support the missions to mars of
+          SpaceX and to give to its community a specific space where they share
+          thought on the missions.
+        </p>
+      </body>
+      <br></br>
       <footer>
         <ul>
-          <li><a href="" target="_blank"><i class="fa fa-facebook fa-fw"></i>Log in</a></li>
-          <li><a href="" target="_blank"><i class="fa fa-instagram fa-fw"></i>Sign up</a></li>
+          <li>
+            <a href="" target="_blank">
+              <i class="fa fa-facebook fa-fw"></i>Sign up
+            </a>
+          </li>
+          <li>
+            <a href="" target="_blank">
+              <i class="fa fa-instagram fa-fw"></i>Log in
+            </a>
+          </li>
         </ul>
         <p>MarsGenX © 2020</p>
       </footer>
--- views/Profile.jsx
+++ views/Profile.jsx
@@ -4,6 +4,34 @@ const Layout = require('./Layout')
 function Profile() {
   return (
     <Layout title="Profile">
+<h1>Profile settings</h1>
+<form id="form-setting" action="edit" method="POST" encType="multipart/form-data">
+
+<label>Nicknamee</label>
+<input type='Nickname' name="Nickname" placeholder="Edit your Nickname" />
+
+<label>Email </label>
+<input type='email' name="email" placeholder="Modify your email" />
+
+<label>Greetings</label>
+<input type='text' name="text" placeholder="choose your greeting" />
+
+<label>Image</label>
+<input type="file" name="profilepic"/>
+
+<button type="submit" className="signup-button">Edit your account</button>
+
+
+{
+            props.errorMessage
+          ? <div className = "error-message">{props.errorMessage}</div>
+          : null
+          }
+
+</form>
+
+
+
 
     </Layout>
   )
